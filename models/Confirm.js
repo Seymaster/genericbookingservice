0721921fa8e0cc7122ed3e42b2e18e9b5d32e464
Seymaster <alugbinoluwaseyi1@gmail.com>
2020-08-19 12:08:28 +0100

    last commit

--- models/Confirm.js
+++ models/Confirm.js
@@ -6,7 +6,8 @@ const Schema      = mongoose.Schema;
 const confirmSchema = new Schema({
     bookingId:       {type: String, required: true},
     userId:          {type: String, required: true},
-    userEmail:       {type: String, required: true}
+    userEmail:       {type: String, required: true},
+    createdAt:       {type: Date, default: Date.now}
 });
 
 const Confirm = mongoose.model('Confirm', confirmSchema);
